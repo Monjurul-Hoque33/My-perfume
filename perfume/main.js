7da66d7fab982ea835b4d5beaa686d1b56c76baa
Monjurul Hoque <monjurulhoque050@gmail.com>
2025-05-30 12:45:39 +0600

    Delete perfume directory

--- perfume/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("hamburger");
-const list = document.getElementById("navlist");
-
-burger.addEventListener("click", ()=>{
-    list.classList.toggle("navlist-active");
-})
